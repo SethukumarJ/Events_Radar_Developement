bb03f3fc63d05a8fc80cce4e80eb20d0a4eeb87e
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-04 23:33:52 +0530

    refactor :1.4
    modelled the schema for storing signing details of jwt and defined the function for validating the expiry of the claims

--- pkg/domain/jwtClaimsModel.go
+++ pkg/domain/jwtClaimsModel.go
@@ -1,8 +1,30 @@
 package domain
 
+import (
+	"fmt"
+	"time"
+
+	"github.com/golang-jwt/jwt"
+)
+
 type JWTClamis struct {
-	UserId uint `json:"userid"`
-	Email string `json:"email"`
-	Role string `json:"role"`
+	UserId uint   `json:"userid"`
+	Email  string `json:"email"`
+	Role   string `json:"role"`
+	jwt.StandardClaims
 }
 
+func (claims JWTClamis) Valid() error {
+	var now = time.Now().UTC().Unix()
+	if claims.VerifyExpiresAt(now, true) {
+		return nil
+	}
+	return fmt.Errorf("Invalid token")
+}
+
+type SignedDetails struct {
+	UserId uint `json:"userid"`
+	UserName string `json:"username"`
+	Role string `json:"role"`
+	jwt.StandardClaims
+}

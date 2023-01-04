faa094ae0b38ad14a1c995a86cfa5987c1468be5
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-04 17:23:17 +0530

    feat :
    modeled the schema for user

--- pkg/domain/user.go
+++ pkg/domain/user.go
@@ -1,7 +1,15 @@
 package domain
 
-type Users struct {
-	ID      uint   `json:"id" gorm:"unique;not null"`
-	Name    string `json:"name"`
-	Surname string `json:"surname"`
+type User struct {
+	UserId       uint   `json:"userid" gorm:"autoIncrement:true;unique"`
+	UserName     string `json:"username" gorm:"primary key;unique" validate:"required,min=2,max=50"`
+	FirstName    string `json:"firstname" validate:"required,min=2,max=50"`
+	LastName     string `json:"lastname" validate:"required,min=1,max=50"`
+	Email        string `json:"email" gorm:"notnull;unique" validate:"email,required"`
+	Password     string `json:"password" validate:"required"`
+	Verification bool   `json:"verification" gorm:"default:false"`
+	Vip          bool   `json:"vip" gorm:"default:false"`
+	PhoneNumber  string `json:"phonenumber" gorm:"unique"`
+	Profile      string `json:"profile"`
+	EventId      uint   `json:"eventid"`
 }

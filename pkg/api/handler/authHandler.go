92caccd1e9944352ccaea87bd684f6768a4217ae
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-05 17:07:29 +0530

    feat
    implemented usersignup function

--- pkg/api/handler/authHandler.go
+++ pkg/api/handler/authHandler.go
@@ -1,13 +1,21 @@
 package handler
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	domain "github.com/thnkrn/go-gin-clean-arch/pkg/domain"
+	"github.com/thnkrn/go-gin-clean-arch/pkg/response"
 	usecase "github.com/thnkrn/go-gin-clean-arch/pkg/usecase/interface"
+	"github.com/thnkrn/go-gin-clean-arch/pkg/utils"
 )
 
 type AuthHandler struct {
 	jwtUserUsecase usecase.JWTUsecase
-	authUsecase usecase.AuthUsecase
-	userUsecase usecase.UserUseCase
+	authUsecase    usecase.AuthUsecase
+	userUsecase    usecase.UserUseCase
 }
 
 func NewAuthHandler(
@@ -15,10 +23,41 @@ func NewAuthHandler(
 	userUsecase usecase.UserUseCase,
 	authUsecase usecase.AuthUsecase,
 
-) *AuthHandler{
+) *AuthHandler {
 	return &AuthHandler{
 		jwtUserUsecase: jwtUserUsecase,
-		authUsecase: authUsecase,
-		userUsecase: userUsecase,
+		authUsecase:    authUsecase,
+		userUsecase:    userUsecase,
 	}
+}
+
+// UserSignup handles the user signup
+
+func (cr *AuthHandler) UserSignup(c *gin.Context) {
+
+	var newUser domain.Users
+	fmt.Println("user signup")
+	//fetching data
+	c.Bind(&newUser)
+
+	//check username exit or not
+
+	err := cr.userUsecase.CreateUser(newUser)
+
+	log.Println(newUser)
+
+	if err != nil {
+		response := response.ErrorResponse("Failed to create user", err.Error(), nil)
+		c.Writer.Header().Add("Content-Type", "application/json")
+		c.Writer.WriteHeader(http.StatusUnprocessableEntity)
+		utils.ResponseJSON(*c, response)
+		return
+	}
+
+	user, _ := cr.userUsecase.FindUser(newUser.Email)
+	response := response.SuccessResponse(true, "SUCCESS", user)
+	c.Writer.Header().Add("Content-Type", "application/json")
+	c.Writer.WriteHeader(http.StatusOK)
+	utils.ResponseJSON(*c, response)
+
 }

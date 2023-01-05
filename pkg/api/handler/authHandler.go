66d21f93bfed8fe250b957f93f9d3dd062774080
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-05 16:56:11 +0530

    feat
    created the instance for athhadler with jwtuseccase, usercase and authusecase

--- pkg/api/handler/authHandler.go
+++ pkg/api/handler/authHandler.go
@@ -1,2 +1,24 @@
 package handler
 
+import (
+	usecase "github.com/thnkrn/go-gin-clean-arch/pkg/usecase/interface"
+)
+
+type AuthHandler struct {
+	jwtUserUsecase usecase.JWTUsecase
+	authUsecase usecase.AuthUsecase
+	userUsecase usecase.UserUseCase
+}
+
+func NewAuthHandler(
+	jwtUserUsecase usecase.JWTUsecase,
+	userUsecase usecase.UserUseCase,
+	authUsecase usecase.AuthUsecase,
+
+) *AuthHandler{
+	return &AuthHandler{
+		jwtUserUsecase: jwtUserUsecase,
+		authUsecase: authUsecase,
+		userUsecase: userUsecase,
+	}
+}

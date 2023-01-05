e8942262ad58a8ee8d8a74818ba523e94c0eb142
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-05 14:02:56 +0530

    refactor:1.9
    implented the models in jwt usecase and it's still unimplemented

--- pkg/usecase/jwtUsecase.go
+++ pkg/usecase/jwtUsecase.go
@@ -1,3 +1,38 @@
 package usecase
 
+import (
+	"os"
 
+	"github.com/thnkrn/go-gin-clean-arch/pkg/domain"
+	usecase "github.com/thnkrn/go-gin-clean-arch/pkg/usecase/interface"
+)
+
+type jwtUsecase struct {
+	SecretKey string
+}
+
+// GenerateRefreshToken implements interfaces.JWTUsecase
+func (*jwtUsecase) GenerateRefreshToken(token string) (string, error) {
+	panic("unimplemented")
+}
+
+// GenerateToken implements interfaces.JWTUsecase
+func (*jwtUsecase) GenerateToken(userid int, email string, role string) string {
+	panic("unimplemented")
+}
+
+// GetTokenFromString implements interfaces.JWTUsecase
+func (*jwtUsecase) GetTokenFromString(signedToken string, claims *domain.SignedDetails) {
+	panic("unimplemented")
+}
+
+// VerifyToken implements interfaces.JWTUsecase
+func (*jwtUsecase) VerifyToken(token string) (bool, *domain.SignedDetails) {
+	panic("unimplemented")
+}
+
+func NewJwtUsecase() usecase.JWTUsecase {
+	return &jwtUsecase{
+		SecretKey: os.Getenv("USER_KEY"),
+	}
+}

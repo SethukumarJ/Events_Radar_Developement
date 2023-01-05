0653b83081ff05ae30dd3017fec4023e8664d649
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-05 21:24:15 +0530

    refactor :3.1
    refactored the config file by adding smtp details

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,20 +1,27 @@
 package config
 
 import (
+	"fmt"
+
 	"github.com/go-playground/validator/v10"
 	"github.com/spf13/viper"
 )
 
 type Config struct {
-	DBHost     string `mapstructure:"DB_HOST"`
-	DBName     string `mapstructure:"DB_NAME"`
-	DBUser     string `mapstructure:"DB_USER"`
-	DBPort     string `mapstructure:"DB_PORT"`
-	DBPassword string `mapstructure:"DB_PASSWORD"`
+	DBHost       string `mapstructure:"DB_HOST"`
+	DBName       string `mapstructure:"DB_NAME"`
+	DBUser       string `mapstructure:"DB_USER"`
+	DBPort       string `mapstructure:"DB_PORT"`
+	DBPassword   string `mapstructure:"DB_PASSWORD"`
+	DBSOURCE     string `mapstructure:"DB_SOURCE"`
+	SMTPPORT     string `mapstructure:"SMTP_PORT"`
+	SMTPHOST     string `mapstructure:"SMTP_HOST"`
+	SMTPPASSWORD string `mapstructure:"SMTP_PASSWORD"`
+	SMTPUSERNAME string `mapstructure:"SMTP_USERNAME"`
 }
 
 var envs = []string{
-	"DB_HOST", "DB_NAME", "DB_USER", "DB_PORT", "DB_PASSWORD",
+	"DB_HOST", "DB_NAME", "DB_USER", "DB_PORT", "DB_PASSWORD", "DB_SOURCE", "SMTP_PORT", "SMTP_HOST", "SMTP_PASSWORD", "SMTP_USERNAME",
 }
 
 func LoadConfig() (Config, error) {
@@ -37,6 +44,6 @@ func LoadConfig() (Config, error) {
 	if err := validator.New().Struct(&config); err != nil {
 		return config, err
 	}
-
+	fmt.Printf("\n\nconfig : %v\n\n", config)
 	return config, nil
 }

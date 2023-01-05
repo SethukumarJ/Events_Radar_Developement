8f600cbbbb59810c0e2104ae33aa5805221bc9d4
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-05 10:43:32 +0530

    chore :1.3
    created a module for response

---
+++ pkg/response/commonResponse.go
@@ -0,0 +1,9 @@
+package response
+
+// Response is used for static shape json return
+type Response struct {
+	Status  bool        `json:"status"`
+	Message string      `json:"message"`
+	Errors  interface{} `json:"errors,omitempty"`
+	Data    interface{} `json:"data,omitempty"`
+}

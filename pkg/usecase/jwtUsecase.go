438fbda0c5301d4604658284d81dbdc7212c38ff
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-05 14:10:42 +0530

    refactor:2.0
    implemented the function to regenerate refresh token

--- pkg/usecase/jwtUsecase.go
+++ pkg/usecase/jwtUsecase.go
@@ -1,8 +1,12 @@
 package usecase
 
 import (
+	"errors"
+	"log"
 	"os"
+	"time"
 
+	"github.com/golang-jwt/jwt"
 	"github.com/thnkrn/go-gin-clean-arch/pkg/domain"
 	usecase "github.com/thnkrn/go-gin-clean-arch/pkg/usecase/interface"
 )
@@ -12,8 +16,24 @@ type jwtUsecase struct {
 }
 
 // GenerateRefreshToken implements interfaces.JWTUsecase
-func (*jwtUsecase) GenerateRefreshToken(token string) (string, error) {
-	panic("unimplemented")
+func (j *jwtUsecase) GenerateRefreshToken(accessToken string) (string, error) {
+	claims := &domain.SignedDetails{}
+	j.GetTokenFromString(accessToken, claims)
+
+	if time.Until(time.Unix(claims.ExpiresAt, 0)) > 30*time.Second {
+		return "", errors.New("too early to generate refresh token")
+	}
+
+	claims.ExpiresAt = time.Now().Local().Add(time.Minute * time.Duration(5)).Unix()
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	refreshToken, err := token.SignedString([]byte(j.SecretKey))
+
+	if err != nil {
+		log.Println(err)
+	}
+	return refreshToken, err
 }
 
 // GenerateToken implements interfaces.JWTUsecase

ba74bcb6bf40c2ccfff4b13182de88f64a1af62f
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-05 22:12:09 +0530

    refactor:3.4
    changed from gorm to sql, and implemented finduser function

--- pkg/repository/userRepository.go
+++ pkg/repository/userRepository.go
@@ -1,35 +1,70 @@
 package repository
 
 import (
+	"database/sql"
+	"fmt"
+
 	domain "github.com/thnkrn/go-gin-clean-arch/pkg/domain"
 	interfaces "github.com/thnkrn/go-gin-clean-arch/pkg/repository/interface"
-	"gorm.io/gorm"
 )
 
-type userDatabase struct {
-	DB *gorm.DB
+type userRepository struct {
+	db *sql.DB
 }
 
 // FindUser implements interfaces.UserRepository
-func (*userDatabase) FindUser(email string) (domain.UserResponse, error) {
-	panic("unimplemented")
+func (c *userRepository) FindUser(email string) (domain.UserResponse, error) {
+
+	var user domain.UserResponse
+
+	query := `SELECT userid,username,firstname,
+			  		lastname,email,password,
+					phonenumber,profileFROM users 
+					WHERE email = $1;`
+
+	err := c.db.QueryRow(query,email).Scan(	&user.UserId,
+											&user.UserName,
+											&user.FirstName,
+											&user.LastName,
+											&user.Email,
+											&user.Password,
+											&user.PhoneNumber,
+											&user.Profile,
+										)							
+
+	fmt.Println("user from find user :", user)
+	return user, err
 }
 
 // InsertUser implements interfaces.UserRepository
-func (*userDatabase) InsertUser(user domain.Users) (int, error) {
+func (*userRepository) InsertUser(user domain.Users) (int, error) {
 	panic("unimplemented")
 }
 
 // StoreVerificationDetails implements interfaces.UserRepository
-func (*userDatabase) StoreVerificationDetails(email string, code int) error {
+func (*userRepository) StoreVerificationDetails(email string, code int) error {
 	panic("unimplemented")
 }
 
 // VerifyAccount implements interfaces.UserRepository
-func (*userDatabase) VerifyAccount(email string, code int) error {
+func (*userRepository) VerifyAccount(email string, code int) error {
 	panic("unimplemented")
 }
 
-func NewUserRepository(DB *gorm.DB) interfaces.UserRepository {
-	return &userDatabase{DB}
+func NewUserRepository(db *sql.DB) interfaces.UserRepository {
+	return &userRepository{
+		db: db,
+	}
 }
+
+// UserId       uint   `json:"userid"`
+// UserName     string `json:"username"`
+// FirstName    string `json:"firstname"`
+// LastName     string `json:"lastname"`
+// Password     string `json:"password"`
+// Email        string `json:"email"`
+// Verification bool   `json:"verification" `
+// Vip          bool   `json:"vip" `
+// PhoneNumber  string `json:"phonenumber"`
+// Profile      string `json:"profile"`
+// Token        string `json:"token"`

0bc30168832d16930faf25af8fcaec8210a483a8
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-06 17:32:48 +0530

    Created verify admin function in the auth usecase

--- pkg/usecase/authUsecase.go
+++ pkg/usecase/authUsecase.go
@@ -2,15 +2,18 @@ package usecase
 
 import (
 	"errors"
+	"fmt"
 	"log"
-	"golang.org/x/crypto/bcrypt"
+
 	repository "github.com/thnkrn/go-gin-clean-arch/pkg/repository/interface"
 	usecase "github.com/thnkrn/go-gin-clean-arch/pkg/usecase/interface"
+	"golang.org/x/crypto/bcrypt"
 )
 
 // authUsecase is the struct for the authentication service
 type authUsecase struct {
-	userRepo repository.UserRepository
+	userRepo  repository.UserRepository
+	adminRepo repository.AdminRepository
 }
 
 func NewAuthUsecase(
@@ -38,14 +41,27 @@ func (c *authUsecase) VerifyUser(email string, password string) error {
 	return nil
 }
 
-// // VerifyPassword verifies the password
-// func VerifyPassword(requestPassword, dbPassword string) bool {
+// VerifyAdmin verifies the admin credentials
+func (c *authUsecase) VerifyAdmin(email, password string) error {
 
-// 	fmt.Println(requestPassword)
-// 	requestPassword = HashPassword(requestPassword)
-// 	fmt.Println(requestPassword)
-// 	return requestPassword == dbPassword
-// }
+	admin, err := c.adminRepo.FindAdmin(email)
+
+	//_, err = c.adminRepo.FindAdmin(email)
+
+	if err != nil {
+		return errors.New("invalid Username/ password, failed to login")
+	}
+
+	fmt.Println("adminpassword", admin.Password)
+	fmt.Println("password:", password)
+
+	isValidPassword := VerifyPassword(admin.Password, []byte(password))
+	if !isValidPassword {
+		return errors.New("invalid username/ Password, failed to login")
+	}
+
+	return nil
+}
 
 func VerifyPassword(hashedPwd string, plainPwd []byte) bool {
 	// Since we'll be getting the hashed password from the DB it

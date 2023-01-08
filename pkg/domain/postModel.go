dc52dc33411bc5b8455f1c990775da1549a2a941
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-08 09:01:43 +0530

    refactor :models
    modelled the schema for the events

--- pkg/domain/postModel.go
+++ pkg/domain/postModel.go
@@ -1,5 +1,7 @@
 package domain
 
+import "time"
+
 type Users struct {
 	UserId       uint   `json:"userid" gorm:"autoIncrement:true;unique"`
 	UserName     string `json:"username" gorm:"primary key;unique" validate:"required,min=2,max=50"`
@@ -26,4 +28,27 @@ type Admins struct {
 	Email        string `json:"email" gorm:"notnull;unique" validate:"email,required"`
 	PhoneNumber  string `json:"phonenumber" gorm:"unique"`
 	Verification bool   `json:"verification" gorm:"default:false"`
+}
+
+type Events struct {
+	EventId                uint      `json:"eventid" gorm:"autoIncrement:true;unique"`
+	Title                  string    `json:"title" gorm:"primary key;unique" validate:"required,min=2,max=50"`
+	OrganizerName          string    `json:"organizername" validate:"required"`
+	EventPic               string    `json:"eventpic" validate:"required"`
+	ShortDiscription       string    `json:"shortdiscription"`
+	LongDiscription        string    `json:"longdiscription"`
+	EventDate              string    `json:"eventdate" validate:"required"`
+	Location               string    `json:"location"`
+	CreatedAt              time.Time `json:"createdat"`
+	Approved               bool      `json:"approved" gorm:"default:false"`
+	Paid                   bool      `json:"paid" gorm:"default:false"`
+	Sex                    string    `json:"sex" gorm:"default:any"`
+	CusatOnly              bool      `json:"cusatonly" gorm:"default:false"`
+	Archived               bool      `json:"archived"`
+	Subevents              string    `json:"subevents"`
+	Online                 bool      `json:"offline" gorm:"default:false"`
+	MaxApplications        int       `json:"maxapplications"`
+	ApplicationClosingDate string    `json:"applicationclosingdate"`
+	ApplicationLink        string    `json:"applicationlink"`
+	WebsiteLink            string    `json:"websitelink"`
 }

033386b4945ec85f6c8c6b84197ed4a2f956b7dd
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-08 09:29:22 +0530

    refactor:1.3
    created Create event function in the event handler

--- pkg/api/handler/eventHandler.go
+++ pkg/api/handler/eventHandler.go
@@ -1,15 +1,52 @@
 package handler
 
 import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	domain "github.com/thnkrn/go-gin-clean-arch/pkg/domain"
+	"github.com/thnkrn/go-gin-clean-arch/pkg/response"
 	usecase "github.com/thnkrn/go-gin-clean-arch/pkg/usecase/interface"
+	"github.com/thnkrn/go-gin-clean-arch/pkg/utils"
 )
 
 type EventHandler struct {
-	eventUseCase usecase.EventUseCase
+	eventUsecase usecase.EventUseCase
 }
 
 func NewEventHandler(usecase usecase.EventUseCase) EventHandler {
 	return EventHandler{
-		eventUseCase: usecase,
+		eventUsecase: usecase,
 	}
+}
+
+func (cr *EventHandler) CreateEvent(c *gin.Context) {
+
+	var newEvent domain.Events
+	fmt.Println("Creating event")
+	//fetching data
+	c.Bind(&newEvent)
+
+	//check username exit or not
+
+	err := cr.eventUsecase.CreateEvent(newEvent)
+
+	log.Println(newEvent)
+
+	if err != nil {
+		response := response.ErrorResponse("Failed to create Event", err.Error(), nil)
+		c.Writer.Header().Add("Content-Type", "application/json")
+		c.Writer.WriteHeader(http.StatusUnprocessableEntity)
+		utils.ResponseJSON(*c, response)
+		return
+	}
+
+	event, _ := cr.eventUsecase.FindEvent(newEvent.Title)
+	response := response.SuccessResponse(true, "SUCCESS", event)
+	c.Writer.Header().Add("Content-Type", "application/json")
+	c.Writer.WriteHeader(http.StatusOK)
+	utils.ResponseJSON(*c, response)
+
 }

2c6c5e152f3c6f94eece07c7eab46a422464d478
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-12 10:30:59 +0530

    feat: fequently asked questions
    implemented schema for the faqa in the post model

--- pkg/domain/postModel.go
+++ pkg/domain/postModel.go
@@ -65,3 +65,12 @@ type Events struct {
 	ApplicationLink        string    `json:"applicationlink"`
 	WebsiteLink            string    `json:"websitelink"`
 }
+
+type Faqa struct {
+	FaqatId       uint      `json:"eventid" gorm:"autoIncrement:true;unique"`
+	Question      string    `json:"question" validate:"required,min=2,max=50"`
+	Answer        string    `json:"answer" validate:"required,min=2,max=50"`
+	Title         string    `json:"title"`
+	CreatedAt     time.Time `json:"createdat"`
+	UserName      string    `json:"username"`
+}

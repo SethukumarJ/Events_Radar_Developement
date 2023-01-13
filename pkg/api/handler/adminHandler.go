2f280bf0053c8784973bda986131c83b406c74c9
SethukumarJ <sethukumarj.76@gmail.com>
2023-01-13 21:21:36 +0530

    refactor:2.2
    implemented function for listing all the organization requests

--- pkg/api/handler/adminHandler.go
+++ pkg/api/handler/adminHandler.go
@@ -31,6 +31,63 @@ func NewAdminHandler(
 	}
 }
 
+// @Summary list all pending organizations for admin
+// @ID list all upcoming events
+// @Tags Admin
+// @Produce json
+// @Security BearerAuth
+// @Param  page   query  int  true  "Page number: "
+// @Param  pagesize   query  int  true  "Page capacity : "
+// @Param  applicationStatus   query  string  true  "List organization based on status: "
+// @Success 200 {object} response.Response{}
+// @Failure 422 {object} response.Response{}
+// @Router /admin/list-organizations [get]
+func (cr *AdminHandler) ListOrgRequests(c *gin.Context) {
+
+
+	page, _ := strconv.Atoi(c.Query("page"))
+
+	pageSize, _ := strconv.Atoi(c.Query("pagesize"))
+
+	applicationStatus:= c.Query("applicationStatus")
+	fmt.Println("applicationStatus",applicationStatus)
+
+	log.Println(page, "   ", pageSize)
+
+	fmt.Println("page :", page)
+	fmt.Println("pagesize", pageSize)
+
+	pagenation := utils.Filter{
+		Page:     page,
+		PageSize: pageSize,
+	}
+
+	fmt.Println("pagenation", pagenation)
+
+	organizations, metadata, err := cr.adminUsecase.ListOrgRequests(pagenation, applicationStatus)
+
+	fmt.Println("events:", organizations)
+
+	result := struct {
+		organizations *[]domain.OrganizationsResponse
+		Meta  *utils.Metadata
+	}{
+		organizations: organizations,
+		Meta:  metadata,
+	}
+
+	if err != nil {
+		response := response.ErrorResponse("error while getting organization applications from database", err.Error(), nil)
+		c.Writer.Header().Add("Content-Type", "application/json")
+		c.Writer.WriteHeader(http.StatusBadRequest)
+		utils.ResponseJSON(*c, response)
+		return
+	}
+
+	response := response.SuccessResponse(true, "Listed All Organization applications", result)
+	utils.ResponseJSON(*c, response)
+
+}
 
 
 // @Summary Resginter the organization
@@ -55,7 +112,7 @@ func (cr *AdminHandler) RegisterOrganization(c *gin.Context)  {
 		utils.ResponseJSON(*c, response)
 		return
 	}
-	response := response.SuccessResponse(true, "Organization registered", orgStatusId)
+	response := response.SuccessResponse(true, "Organization rejected", orgStatusId)
 	utils.ResponseJSON(*c, response)
 
 }

3b90e6e3d1b156d805d56d2833b984bbf4ddb8cd
Sethukumar J <sethukumarj.76@gmail.com>
2023-03-05 20:26:30 +0530

    modified the host in swagger

--- cmd/api/docs/docs.go
+++ cmd/api/docs/docs.go
@@ -2443,7 +2443,7 @@ const docTemplate = `{
 // SwaggerInfo holds exported Swagger Info so clients can modify it
 var SwaggerInfo = &swag.Spec{
 	Version:          "1.0",
-	Host:             "localhost:3000",
+	Host:             "eventsradar.online",
 	BasePath:         "/",
 	Schemes:          []string{},
 	Title:            "Go + Gin Radar API",
